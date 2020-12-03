91d9391fee45c116489062a9a80113f11a6db826
Panikos Christou <panikoschristou@yahoo.com>
2020-12-03 09:55:58 +0200

    test

--- src/application/DbFunctions.java
+++ src/application/DbFunctions.java
@@ -1,28 +1,31 @@
 package application;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DbFunctions {
 	//GET LOC HASHES
-	public User convertObjArrToUser(Object[] obj) {
-		char gender = 'M';
-		if (!(boolean)obj[7]) {
-			gender = 'F';
-		}
-		boolean isVerified = false;
-		if (!(boolean)obj[11]) {
-			isVerified = true;
-		}
-		return new User((int)obj[0], (String) obj[1], (String) obj[2], (String) obj[3], 
-				(String) obj[4], (String) obj[5], (Timestamp) obj[6], gender, null, null, null, isVerified, hometown, livesInLocation)
-				
-		
-	}
+//	public User convertObjArrToUser(Object[] obj) {
+//		char gender = 'M';
+//		if (!(boolean)obj[7]) {
+//			gender = 'F';
+//		}
+//		boolean isVerified = false;
+//		if (!(boolean)obj[11]) {
+//			isVerified = true;
+//		}
+//		return new User((int)obj[0], (String) obj[1], (String) obj[2], (String) obj[3], 
+//				(String) obj[4], (String) obj[5], (Timestamp) obj[6], gender, null, null, null, isVerified, hometown, livesInLocation)
+//				
+//		
+//	}
+	static HashMap<String, Integer> locationHashmap = SearchUserController.getLocations();
 	public static List<Object[]> resultSetToList(ResultSet rs){
 		List<Object[]> results = new ArrayList<Object[]>();
 		try {
@@ -80,8 +83,15 @@ public class DbFunctions {
 
 	}
 	public static User covertObjectArray(Object[] temp) {
-		return null;
-//		return new User((int) temp[0], (String) temp[1], (String) temp[2], (String) temp[3], temp[4].toString(),
-//				(boolean) (temp[7]), (String) temp[5], (String) temp[6]);
+		
+	return new User((String) temp[3],(String) temp[4],
+			(String) temp[5], (String) temp[6], (String) temp[7],
+			(Date) temp[8], (boolean)temp[9],(ArrayList<String>) null,(ArrayList<String>) null,
+			(ArrayList<String>) null, (boolean)temp[10],
+			new Location(-1,""),
+			new Location(-1,""),
+			"", "");
+
+
 	}
 }
--- src/application/SearchUserModel.java
+++ src/application/SearchUserModel.java
@@ -1,11 +1,29 @@
 package application;
 
 import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SearchUserModel {
+
+	public static ResultSet getLocations() {
+		String SPsql = "EXEC retrieveLocations "; // for stored proc taking 2 parameters
+		ResultSet resultSet = null;
+		try {
+			PreparedStatement ps = AuthenticationModel.conn.prepareStatement(SPsql);
+			ps.setEscapeProcessing(true);
+			resultSet = ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		return resultSet;
+	}
 
 	SearchUserController controller;
 
@@ -17,13 +35,41 @@ public class SearchUserModel {
 		try {
 			List<Object[]> res = new ArrayList<Object[]>();
 			ResultSet rs = null;
-			CallableStatement cstmt = AuthenticationModel.conn.prepareCall(
-					"{call searchUsers(?,?,?,?,?,?,?,?,?,?,?,?,?,?)}", ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_READ_ONLY);
+			String firstName = newUser.firstName;
+			String lastName = newUser.lastName;
+			String email = newUser.email;
+			String website = newUser.website;
+			String link = newUser.link;
+			Date birthday = newUser.birthday;
+			boolean gender = newUser.gender;
+			String workedFor = AuthenticationModel.translateArrayListToString(newUser.workedFor);
+			String educationPlaces = AuthenticationModel.translateArrayListToString(newUser.educationPlaces);
+			String quotes = AuthenticationModel.translateArrayListToString(newUser.quotes);
+			boolean isVerified = newUser.isVerified;
+			int hometownFK = newUser.hometown.getId();
+			int livesInLocationFK = newUser.livesInLocation.getId();
+			// newer with pass and username
+			String username = newUser.username;
 
-			cstmt.setInt("USERID", newUser.getId());
-			cstmt.setArray("nvarchar(50)", newUser.educationPlaces);
-			// ...
+			CallableStatement cstmt = AuthenticationModel.conn.prepareCall(
+					"{call SEARCH_USERS_TEMP2(?,?,?,?, ?,?,?,?, ?,?,?,?, ?,?)}", ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			int columnIndex = 1;
+			cstmt.setString(columnIndex++, firstName);
+			cstmt.setString(columnIndex++, lastName);
+			cstmt.setString(columnIndex++, username);
+			cstmt.setString(columnIndex++, email);
+			cstmt.setString(columnIndex++, website);
+			cstmt.setString(columnIndex++, link);
+			cstmt.setDate(columnIndex++, birthday);
+			cstmt.setBoolean(columnIndex++, gender);
+			cstmt.setString(columnIndex++, workedFor);
+			cstmt.setString(columnIndex++, educationPlaces);
+			cstmt.setString(columnIndex++, quotes);
+			cstmt.setBoolean(columnIndex++, isVerified);
+			cstmt.setInt(columnIndex++, hometownFK);
+			cstmt.setInt(columnIndex++, livesInLocationFK);
+
 			boolean results = cstmt.execute();
 			int rowsAffected = 0;
 
@@ -38,14 +84,19 @@ public class SearchUserModel {
 				results = cstmt.getMoreResults();
 			}
 			List<Object[]> objs = DbFunctions.resultSetToList(rs);
-			
-			
+			User[] ret = new User[objs.size()];
+
+			for (int i = 0; i < ret.length; i++) {
+				ret[i] = DbFunctions.covertObjectArray(objs.get(i));
+			}
+			System.out.println(ret);
+			return ret;
+
 		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 
-		return null;
 	}
 
 	public void setController(SearchUserController searchUserController) {
